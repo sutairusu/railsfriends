c2e64e93f8793156bdb52ebdd32c63878942d194
sutairusu <120032637+sutairusu@users.noreply.github.com>
2022-12-29 15:09:36 +0900

    yet another adjustment

--- db/migrate/20221226101938_create_friends.rb
+++ db/migrate/20221226101938_create_friends.rb
@@ -1,11 +1,11 @@
 class CreateFriends < ActiveRecord::Migration[7.0]
   def change
     create_table :friends do |t|
-      t.text :first_name
-      t.text :last_name
-      t.text :email
-      t.text :phone
-      t.text :twitter
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :twitter
 
       t.timestamps
     end
